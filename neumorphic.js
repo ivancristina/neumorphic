86ee7bd14a553640979bc9f483cc33046266e715
Ivan Cristina <ivancristina5@gmail.com>
2020-01-23 05:12:24 +0100

    New section and smoother transitions

--- neumorphic.js
+++ neumorphic.js
@@ -1,6 +1,7 @@
 function home() {
     $('#pay').collapse('hide');
     $('#home').collapse('show');
+    $('#info').collapse('hide');
     $('#section1').addClass('neofooteractive');
     $('#section2').removeClass('neofooteractive');
     $('#section3').removeClass('neofooteractive');
@@ -10,15 +11,17 @@ function home() {
 function pay() {
     $('#home').collapse('hide');
     $('#pay').collapse('show');
+    $('#info').collapse('hide');
     $('#section1').removeClass('neofooteractive');
     $('#section2').addClass('neofooteractive');
     $('#section3').removeClass('neofooteractive');
 }
 
 function info() {
-    $('#home').collapse('toggle');
-    $('#pay').collapse('toggle');
+    $('#home').collapse('hide');
+    $('#pay').collapse('hide');
+    $('#info').collapse('show');
     $('#section1').removeClass('neofooteractive');
     $('#section2').removeClass('neofooteractive');
-    //$('#section3').addClass('neofooteractive');
+    $('#section3').addClass('neofooteractive');
 }
